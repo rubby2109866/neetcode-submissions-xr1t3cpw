f14f4404ff38b2b36e4a1015cfcfd9f930d18990
Rushabh Shah <59765473+rubby2109866@users.noreply.github.com>
2026-04-10 12:56:24 -0400

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        Map<Character, Character> brackets = new HashMap<>();
+        brackets.put('}', '{');
+        brackets.put(')', '(');
+        brackets.put(']', '[');
+
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (brackets.containsKey(c)) {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char closeParan = brackets.get(c);
+                char openParan = stack.pop();
+                if (closeParan != openParan) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
